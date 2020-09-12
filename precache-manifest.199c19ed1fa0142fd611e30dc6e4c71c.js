f8c1dfaa0a0741c762f13b6e03aa48288c208bf7
naisarg53 <naisarg.pandya1998@gmail.com>
2020-09-12 10:42:49 +0530

    Updates

---
+++ precache-manifest.199c19ed1fa0142fd611e30dc6e4c71c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ce20629bff94be83b0aca576f12281",
+    "url": "/COVID-19_tracker_reactjs/index.html"
+  },
+  {
+    "revision": "7c6a45284dde14e6b271",
+    "url": "/COVID-19_tracker_reactjs/static/css/main.fd804e76.chunk.css"
+  },
+  {
+    "revision": "2213f98df3679b981da4",
+    "url": "/COVID-19_tracker_reactjs/static/js/2.12a73cfa.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/COVID-19_tracker_reactjs/static/js/2.12a73cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6a45284dde14e6b271",
+    "url": "/COVID-19_tracker_reactjs/static/js/main.82fadda3.chunk.js"
+  },
+  {
+    "revision": "b98de6c893006423792a",
+    "url": "/COVID-19_tracker_reactjs/static/js/runtime-main.877e1ae0.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19_tracker_reactjs/static/media/image.d7265326.png"
+  }
+]);
